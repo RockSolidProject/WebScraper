ecb8c33e2cfe1efeae179dd24e13727bba6b5140
tzorin4 <106032629+tzorin4@users.noreply.github.com>
2025-05-15 09:05:08 +0200

    ROCK-47: added scrapper for inner climbing spots

--- src/main/kotlin/MainScrape.kt
+++ src/main/kotlin/MainScrape.kt
@@ -1,16 +1,8 @@
-import webScraper.KspScraper
-import webScraper.PlezanjeScraper
+import webScraper.InnerClimbingCenter.KspScraper
 import java.io.File
 
 fun main() {
-    println(PlezanjeScraper().getData1())
-    println(KspScraper().getData1())
-    val climbingSpot = PlezanjeScraper().scrapeAllClimbingSpots()
-    val output = PlezanjeScraper().climbingSpotsToString(climbingSpot)
-
-    // Save the output to a file
-    File("climbing_spots.txt").writeText(output)
-    println("Climbing spots saved to climbing_spots.txt")
-
-
+    KspScraper().getData1().forEach {
+        println(it)
+    }
 }
