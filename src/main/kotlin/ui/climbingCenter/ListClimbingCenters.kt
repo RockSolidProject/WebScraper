c328059b47941429b29c6d29bdf1a83d3c46685a
UrbanLevicar <urban.levicar@student.um.si>
2025-05-24 12:35:22 +0200

    ROCK-96: Added filtering functionality and added shading for selected tab

--- src/main/kotlin/ui/climbingCenter/ListClimbingCenters.kt
+++ src/main/kotlin/ui/climbingCenter/ListClimbingCenters.kt
@@ -1,22 +1,16 @@
 package ui.climbingCenter
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import webScraper.InnerClimbingCenter.InnerClimbingCenter
 
 @Preview
@@ -27,18 +21,68 @@ fun ListClimbingCenter(
 ) {
     var selectedCenter by remember { mutableStateOf<InnerClimbingCenter?>(null) }
     var isDialogOpen by remember { mutableStateOf(false) }
+    var filteredClimbingCenters by remember { mutableStateOf(climbingCenters) }
+    var filterText by remember { mutableStateOf("") }
 
-    GridClimbingCenters(
-        climbingCenters,
-        onCardClick = { center ->
-        selectedCenter = center
-        isDialogOpen = true
-        },
-        onDeleteClick = { centerToDelete ->
-            val updatedCenters = climbingCenters.filter { it != centerToDelete }
-            onUpdate(updatedCenters)
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = filterText,
+                onValueChange = {
+                    filterText = it
+                    filteredClimbingCenters = filterClimbingCenters(climbingCenters, filterText)
+                },
+                label = { Text("Filter") },
+                modifier = Modifier.weight(1f),
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                modifier = Modifier
+                    .clickable {
+                        // TODO("Shranjevanje v bazo")
+                    }
+                    .background(Color(0xFF0288D1), shape = MaterialTheme.shapes.medium)
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Save to database",
+                    color = Color.White
+                )
+            }
         }
-    )
+        GridClimbingCenters(
+            climbingCenters = filteredClimbingCenters,
+            onCardClick = { center ->
+                selectedCenter = center
+                isDialogOpen = true
+            },
+            onDeleteClick = { centerToDelete ->
+                val updatedCenters = climbingCenters.filter { it != centerToDelete }
+                onUpdate(updatedCenters)
+            }
+        )
+    }
+
+    if (isDialogOpen && selectedCenter != null) {
+        EditClimbingCenterDialog(
+            center = selectedCenter!!,
+            onDismiss = { isDialogOpen = false },
+            onSave = { updatedCenter ->
+                val updatedList = climbingCenters.map {
+                    if (it == selectedCenter) updatedCenter else it
+                }
+                onUpdate(updatedList)
+                isDialogOpen = false
+            }
+        )
+    }
 
     if (isDialogOpen && selectedCenter != null) {
         EditClimbingCenterDialog(
@@ -55,141 +99,10 @@ fun ListClimbingCenter(
     }
 }
 
-@Composable
-fun GridClimbingCenters(
-    climbingCenters: List<InnerClimbingCenter>,
-    onCardClick: (InnerClimbingCenter) -> Unit,
-    onDeleteClick: (InnerClimbingCenter) -> Unit
-) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        items(climbingCenters) { center ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f)
-                    .clickable { onCardClick(center) },
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFFE1F5FE)
-                ),
-                shape = RoundedCornerShape(16.dp),
-                elevation = CardDefaults.cardElevation(6.dp),
-                border = BorderStroke(1.dp, Color(0xFF0288D1))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.Top
-                    ) {
-                        Text(
-                            text = center.name,
-                            style = MaterialTheme.typography.titleMedium,
-                            color = Color(0xFF01579B),
-                            maxLines = 3,
-                            overflow = TextOverflow.Ellipsis,
-                            modifier = Modifier.weight(1f)
-                        )
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Delete",
-                            tint = Color.Red,
-                            modifier = Modifier
-                                .padding(start = 8.dp)
-                                .clickable { onDeleteClick(center) }
-                        )
-                    }
-                    Column {
-                        Text(
-                            text = "Lat: ${center.latitude}",
-                            style = MaterialTheme.typography.labelMedium,
-                            color = Color.DarkGray,
-                            fontSize = 15.sp
-                        )
-                        Text(
-                            text = "Lng: ${center.longitude}",
-                            style = MaterialTheme.typography.labelMedium,
-                            color = Color.DarkGray,
-                            fontSize = 15.sp
-                        )
-                    }
-                }
-            }
-        }
+fun filterClimbingCenters(climbingCenters: List<InnerClimbingCenter>, filterText: String): List<InnerClimbingCenter>{
+    if (filterText == "" || climbingCenters.isEmpty()) return climbingCenters
+    return climbingCenters.filter {it.name.contains(filterText, true) ||
+            it.latitude.toString().contains(filterText, true) ||
+            it.longitude.toString().contains(filterText, true)
     }
-}
-
-@Composable
-fun EditClimbingCenterDialog(
-    center: InnerClimbingCenter,
-    onDismiss: () -> Unit,
-    onSave: (InnerClimbingCenter) -> Unit
-) {
-    var name by remember { mutableStateOf(center.name) }
-    var latitude by remember { mutableStateOf(center.latitude.toString()) }
-    var longitude by remember { mutableStateOf(center.longitude.toString()) }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text("Edit Climbing Center") },
-        containerColor = Color(0xFFE1F5FE),
-        text = {
-            Column {
-                OutlinedTextField(
-                    value = name,
-                    onValueChange = { name = it },
-                    label = { Text("Name") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                OutlinedTextField(
-                    value = latitude,
-                    onValueChange = { latitude = it },
-                    label = { Text("Latitude") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                OutlinedTextField(
-                    value = longitude,
-                    onValueChange = { longitude = it },
-                    label = { Text("Longitude") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-        },
-        confirmButton = {
-            Button(
-                colors = ButtonDefaults.buttonColors(Color(0xFF01579B)),
-                onClick = {
-                val updatedCenter = InnerClimbingCenter(
-                    name = name,
-                    latitude = latitude.toDoubleOrNull() ?: center.latitude,
-                    longitude = longitude.toDoubleOrNull() ?: center.longitude
-                )
-                onSave(updatedCenter)
-            }) {
-                Text("Save")
-            }
-        },
-        dismissButton = {
-            Button(
-                onClick = onDismiss,
-                colors = ButtonDefaults.buttonColors(Color(0xFF01579B)),
-                ) {
-                Text("Cancel")
-            }
-        }
-    )
 }
